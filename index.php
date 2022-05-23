f5dc423b45e2a55662c3ef6559994cf287b94c4d
Mambawih <songate.isaac@gmail.com>
2022-05-23 13:01:56 +0530

    added index.php, img folder and css/style.css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype>
+<html lang="en">
+    <head>
+        <meta lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Portfolio site</title>
+        <link style="border-radius:50%;" rel="icon" href="img/avatar.jpg">
+    </head>
+    <body>
+        <header class="header">
+            <div class="header-items">
+                <img src="img/avatar.jpg" alt="logo">
+                <nav class="navbar">
+                    <li class="nav-items"><a href="#">Home</a></li>
+                    <li class="nav-items"><a href="#">About</a></li>
+                    <li class="nav-items"><a href="#">project</a></li>
+                    <li class="nav-items"><a href="#">contact</a></li>
+                </nav>
+            </div>
+        </header>
+    </body>
+</html>
