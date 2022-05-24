b5b5920cc105daa422fa0a51646f4019c5ca6828
Mambawih <songate.isaac@gmail.com>
2022-05-24 22:32:11 +0530

    just a few changes

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
-<!doctype>
-<html lang="en">
-    <head>
-        <meta lang="en">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/style.css">
-        <title>Portfolio site</title>
-        <link style="border-radius:50%;" rel="icon" href="img/avatar.jpg">
-    </head>
-    <body>
-        <header class="header">
-            <div class="header-items">
-                <img src="img/avatar.jpg" alt="logo">
-                <nav class="navbar">
-                    <li class="nav-items"><a href="#">Home</a></li>
-                    <li class="nav-items"><a href="#">About</a></li>
-                    <li class="nav-items"><a href="#">project</a></li>
-                    <li class="nav-items"><a href="#">contact</a></li>
-                </nav>
-            </div>
-        </header>
-    </body>
-</html>
+<?php
+include "header.php"
+?>
+
+<section class="body">
+    <div>
+        <div>
+            <form class="form" action="submit.php">
+                <input class="input-field" type="text" name="name" placeholder="Name" required><br>
+                <input class="input-field" type="text" name="text" placeholder="Text" required><br>
+                <input class="input-field" type="text" name="content" placeholder="Anything" required><br>
+                <input class="input-submit" type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+</section>
+
+<?php
+include "footer.php"
+?>
