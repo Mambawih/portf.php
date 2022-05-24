9994df7b08557cca0ebba9cd5033fac9b942f7bc
Mambawih <songate.isaac@gmail.com>
2022-05-24 22:34:39 +0530

    added footer.php and header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Portfolio site</title>
+        <link style="border-radius:50%;" rel="icon" href="img/avatar.jpg">
+    </head>
+    <body>
+        <section class="header">
+            <div class="header-items">
+                <img src="img/avatar.jpg" alt="logo">
+                <nav class="navbar">
+                    <li class="nav-items"><a href="#">Home</a></li>
+                    <li class="nav-items"><a href="#">About</a></li>
+                    <li class="nav-items"><a href="#">project</a></li>
+                    <li class="nav-items"><a href="#">contact</a></li>
+                </nav>
+            </div>
+        </section>
