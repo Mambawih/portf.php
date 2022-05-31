08e481c8337fb6d03c5d929d654cc6c42fb876c3
Mambawih <songate.isaac@gmail.com>
2022-05-31 13:38:54 +0530

    edited function.php

--- function.php
+++ function.php
@@ -33,4 +33,13 @@ function time_elapsed_string($datetime, $full = false) {
     if (!$full) $string = array_slice($string, 0, 1);
     return $string ? implode(', ', $string) . ' ago' : 'just now';
 }
+
+function redirect($link){
+	?>
+	<script>
+	window.location.href='<?php echo $link?>';
+	</script>
+	<?php
+	die();
+}
 ?>
