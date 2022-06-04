308de79b581e3d75e4de3b99b252bd673323d9e9
Mambawih <songate.isaac@gmail.com>
2022-06-04 23:14:11 +0530

    added manage_project.php

---
+++ mine/manage_project.php
@@ -0,0 +1,10 @@
+<?php
+include 'header.php';
+?>
+
+<section class="container">
+    <form action="" method="post"></form>
+    <input type="text" required placeholder="Name of project">
+    <input type="text" required placeholder="About the project">
+    <!-- to be continued -->
+</section>
