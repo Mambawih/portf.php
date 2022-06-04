c8583a5f04c651ebc93a6b49f1197bdffe427744
Mambawih <songate.isaac@gmail.com>
2022-06-04 23:17:38 +0530

    added input fields (incomplete)

--- mine/manage_project.php
+++ mine/manage_project.php
@@ -6,5 +6,7 @@ include 'header.php';
     <form action="" method="post"></form>
     <input type="text" required placeholder="Name of project">
     <input type="text" required placeholder="About the project">
+    <input type="text" required placeholder="About the project">
+    <input type="text" required placeholder="About the project">
     <!-- to be continued -->
 </section>
