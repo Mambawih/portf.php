287a9ca8c155a9bf5b93b171e0975e60f275a266
Mambawih <songate.isaac@gmail.com>
2022-06-04 23:10:11 +0530

    added toggle functionality for admin index page

--- js/mains.js
+++ js/mains.js
@@ -34,3 +34,23 @@ function readMore() {
   //     moreText.style.display = "inline";
   //   }
   // } 
+
+
+// toggle menu in admin page
+// message, blogs, projects...
+  function showPage(page){
+
+    document.querySelectorAll('div').forEach(div =>{
+      div.style.display = 'none';
+    })
+
+    document.querySelector(`#${page}`).style.display = 'block';
+  }
+
+  document.addEventListener('DOMContentLoaded',function(){
+    document.querySelectorAll('button').forEach(button =>{
+      button.onclick = function(){
+        showPage(this.dataset.page);
+      }
+    })
+  })
